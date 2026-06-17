4284ff0113989a209f02c0a13b27c1e22266e684
Yunliu <yunliu_zz@outlook.com>
2026-06-17 14:37:56 +0800

    更新config和MyBaseTask

--- src/tasks/MyBaseTask.py
+++ src/tasks/MyBaseTask.py
@@ -6,6 +6,11 @@ class MyBaseTask(BaseTask):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.name = "基本设置"
+        self.description = "切换御魂 判断是否在主页"
+    def in_home(self):
+        return self.find_one(['Home_Town', 'Home_Store', 'Home_Sign'],
+                            threshold=0.6) is not None
 
 
 
